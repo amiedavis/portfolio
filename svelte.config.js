032633582b6f05d908bf158e19e21d7f0d621114
Amie Davis <amiedav@gmail.com>
2024-06-19 11:53:34 +0800

    Config for static page, WIP

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,27 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+// TODO: set up a 404 page
+// TODO: move app template back to app.html default
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined, 
+			precompress: false,
+			strict: true
+		}),
 		files: {
 			appTemplate: "index.html",
-		}
-	}
+		},
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        },
+	},
+	preprocess: vitePreprocess(),
   };
 
 export default config;
