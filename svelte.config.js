9516e75410c9c65d9a9abdd21f56e471b3593270
Amie Davis <amiedav@gmail.com>
2024-06-20 14:20:51 +0800

    Update appDir in config

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		appDir: 'app',
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
