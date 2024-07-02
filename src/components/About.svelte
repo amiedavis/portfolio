0161da790f3671281d9b458a38de50ed42389d67
Amie Davis <amiedav@gmail.com>
2024-07-02 14:13:49 +0800

    Update copy

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -24,18 +24,15 @@
     <h2>Who am I?</h2>
     <hr/>
     <p>
-        Isn’t that a great question, and one I think we should all ask ourselves on a regular basis 😉<br><br>
-        First and foremost I am a partner, aunty, lover of pottery and poetry, keen amateur futsal player and Matildas supporter.<br><br>
-        I’m also a full-stack engineer with 7 years experience. I got my start back in uni - I was doing my Engineering degree at the Australian National University when I stumbled upon coding in my second year and thought, "Hey, this is pretty cool!" Even though I ended up majoring in mechatronics (because, well, microprocessors are fun), that coding bug never really left me.<br><br>
-        Over the years, I've picked up a few extra skills. I've got a knack for writing software documentation and enjoy the challenge of communicating technical concepts to non-technical audiences. I also love the problem solving involved in breaking down big technical projects into smaller, well-defined tasks, and have more recently taken opportunities to lean more into a Business Analyst role than pure Software Engineering.
+        I’m a full-stack software engineer with over 7 years experience and a bachelor’s degree in Engineering (majoring in mechatronics).<br><br>
+        Over the years, I've developed a diverse skillset that goes beyond writing code. I have lots of experience in writing software documentation and enjoy the challenge of explaining technical concepts to non-technical audiences. I also love the problem solving involved in breaking down big technical projects into smaller, well-defined tasks, and have more recently taken opportunities to lean more into a Business Analyst role than pure Software Engineering.
     </p>
 
     <h2>Work with me</h2>
     <hr>
     <p>
-        Right now I’m working on building up my portfolio, since most of my work life has consisted of contributing to proprietary software. The cool part? I'm offering lower rates at the moment, which is great if you're working with a tight budget.<br><br>
-        Got an app idea you've been sitting on? Need a website that doesn't look like it's stuck in the 90s? Or maybe you just want someone to help you figure out what you want, with no strings attached? Well, that's where I come in! <br><br>
-        I'm all about turning those 'wouldn't it be nice if...' thoughts into actual, working tech. So if you're curious about what we could create together, let's chat! No pressure, just good ideas and maybe some nerdy jokes thrown in for free.
+        Whether you are looking for someone to help build you a website, need to improve your user manuals or just want someone to help you figure out what direction to go in with a big idea, I’m your gal!<br><br>
+        Right now I’m working on building up my portfolio, since most of my work life has consisted of contributing to proprietary software. This means I’m offering lower rates at the moment, so even if you’re working with a tight budget, I’d love to hear from you!
     </p>
 
     <form id="contact-form" action="https://formspree.io/f/myyrrard" accept-charset="utf-8" method="POST">
